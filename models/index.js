946f6e9267e05c2aa27b60adab229f2b9b01907b
Richard Murphy <richinatl@gmail.com>
2022-04-08 19:57:58 -0400

    adding columns for models

--- models/index.js
+++ models/index.js
@@ -6,11 +6,30 @@ const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
+
 // Categories have many Products
+
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: "product_id" });
+
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: "product_id" });
+
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: "tag_id" });
 
 module.exports = {
   Product,
