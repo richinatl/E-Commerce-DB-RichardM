85cfd0c02ea40da3ad7526df709f5037cda2346f
Richard Murphy <richinatl@gmail.com>
2022-04-11 16:41:00 -0400

    added missed delete route for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -110,8 +110,22 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product found with that ID" });
+      return;
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
